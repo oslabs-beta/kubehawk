595f1611d20ef2c89c293fe03a190c23ef7e3e08
Yomkool <bannon.pj@gmail.com>
2023-04-03 15:49:01 -0400

    Update Reactdom render method and install react spring dependencies

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './src/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+createRoot(rootElement).render(<App />);
