82fc8d932523ce98621ff7f9f3b6819dfb3cbe84
sffw <spencerffwhite@gmail.com>
2023-04-14 14:37:46 -0400

    Condense file structure moving next-dash into root directory

--- next-dash/node_modules/next/dist/server/initialize-require-hook.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var _requireHook = require("../build/webpack/require-hook");
-(0, _requireHook).loadRequireHook();
-const isPrebundled = true;
-if (isPrebundled) {
-    (0, _requireHook).overrideBuiltInReactPackages();
-}
-
-//# sourceMappingURL=initialize-require-hook.js.map
