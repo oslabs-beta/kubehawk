e042b591bad5bf019ca5f813b69cd2f4033d82de
Kyle Brandt <knbrandt47@gmail.com>
2023-04-15 12:37:47 -0400

    re-completed splash info components to show product info

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,26 +1,49 @@
-import React from 'react'
-import Navbar from './Navbar'
-import loginImg from './assets/login_4.png'
-import LandCopy from './LandCopy'
-import GetKlusterInfo from './GetKlusterInfo'
-import { Login } from './Login'
+import React from "react";
+import Navbar from "./Navbar";
+import loginImg from "./assets/login_4.png";
+import LandCopy from "./LandCopy";
+import SplashInfoLeft from "./SplashInfoLeft";
+import SplashInfoRight from "./SplashInfoRight";
+import GetKlusterInfo from "./GetKlusterInfo";
+import { Login } from "./Login";
 
 function Landing() {
-  const navItems = ['Login', 'help'].map((item, i)=>{
+  const navItems = ["Login", "help"].map((item, i) => {
     return <li key={item + i}>{item}</li>;
-  })
+  });
+
+  const infoItems = [
+    {
+      "header": "Save money with Kuberenetes!",
+      "text": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste nemo atque veniam aut! Itaque architecto nihil repudiandae minus perspiciatis, dolores quaerat ullam, recusandae beatae et quasi a optio illum veniam!",
+    },
+    {
+      "header": "Save money with Kuberenetes!",
+      "text": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste nemo atque veniam aut! Itaque architecto nihil repudiandae minus perspiciatis, dolores quaerat ullam, recusandae beatae et quasi a optio illum veniam!",
+    },
+    {
+      "header": "Save money with Kuberenetes!",
+      "text": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste nemo atque veniam aut! Itaque architecto nihil repudiandae minus perspiciatis, dolores quaerat ullam, recusandae beatae et quasi a optio illum veniam!",
+    },
+
+  ]
 
   return (
-    <div className='landing' >
-            <Navbar navItems={navItems} kubeButton = {<button>Get KubeHawk</button>}/>
-          <div id="landing-login">
-            <LandCopy />
-            <div id='login-image'>
-              <img src={loginImg} alt="security_login" />
-            </div>
-            </div>
+    <div className="landing">
+      <Navbar navItems={navItems} kubeButton={<button>Get KubeHawk</button>} />
+      <div id="landing-body">
+        <div id="landing-login">
+          <LandCopy />
+          <div id="login-image">
+            <img src={loginImg} alt="security_login" />
+          </div>
+        </div>
+        <SplashInfoLeft header={infoItems[0].header} text={infoItems[0].text} image={loginImg}></SplashInfoLeft>
+        <SplashInfoRight header={infoItems[1].header} text={infoItems[1].text} image={loginImg}></SplashInfoRight>
+        <SplashInfoLeft header={infoItems[2].header} text={infoItems[2].text} image={loginImg}></SplashInfoLeft>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Landing
+export default Landing;
---
+++ src/components/SplashInfoLeft.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const SplashInfoLeft = (props) => {
+    return (
+      <div className="splashleft">
+        <div id="splash.info">
+          <h1 id="splash.header">
+            {props.header}
+          </h1>
+          <div id="splash.text">
+            {props.text}
+          </div>
+        </div>
+        <img src={props.image} className="splashleftimg" alt="A picture" />
+      </div>
+    )
+}
+
+export default SplashInfoLeft;
