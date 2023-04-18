162a1d82c4c110a2f892b82adafb54105c5740ef
stevenox7 <101216881+stevenox7@users.noreply.github.com>
2023-04-18 10:58:31 -0400

    Added useContext and made the create new tabs function

--- components/addClusterModal.tsx
+++ components/addClusterModal.tsx
@@ -1,91 +1,103 @@
 import React, { ChangeEvent, MouseEvent, useContext } from "react";
 import { Modal, Button, Text, Input, Row, Checkbox } from "@nextui-org/react";
 import StateContext from "../context/StateContext";
+import { StateContextType } from "../context/StateContext";
 interface AppState {
     clusters: { ipAddress: string; clusterName: string; }[];
     currentTab: { ipAddress: string; clusterName: string; };
   }
   
-  const initialState: AppState = {
-    clusters: [],
-    currentTab: { ipAddress: '', clusterName: '' }
-  };
+//   const initialState: AppState = {
+//     clusters: [],
+//     currentTab: { ipAddress: '', clusterName: '' }
+//   };
   
-export default function ClusterModal() {
-  const [visible, setVisible] = React.useState(false);
-  const handler = () => setVisible(true);
-
-  const [ipAddress, setIpAddress]: [string, React.Dispatch<React.SetStateAction<string>>] = React.useState('')
-  const [clusterName, setClusterName]: [string, React.Dispatch<React.SetStateAction<string>>] = React.useState('')
-
-  const { componentState, setComponentState } = useContext(StateContext)
-  const handleIpAddressChange = (e: ChangeEvent<HTMLInputElement>): void => {
-    setIpAddress(e.target.value);
-  };
-  const handleClusterNameChange = (e: ChangeEvent<HTMLInputElement>): void => {
-    console.log(e.target.value)
-    setClusterName(e.target.value);
-  };
-  const closeHandler= ((prevState) => ({
-    ...prevState,
-    clusters: [...prevState.clusters, {ipAddress, clusterName}],
-    currentTab: {
-      ipAddress: ipAddress,
-      clusterName: clusterName
-    }
-  }));
-
-  return (
-    <div>
-      <Button auto shadow onPress={handler}>
-        Add Clusters
-      </Button>
-      <Modal
-        closeButton
-        aria-labelledby="modal-title"
-        open={visible}
-        onClose={() => }
-      >
-        <Modal.Header>
-          <Text id="modal-title" size={18}>
-            KubeHawk
-            <Text b size={18}>
-             
+  export default function ClusterModal() {
+    const [visible, setVisible] = React.useState(false);
+    const handler = () => setVisible(true);
+  
+    const [ipAddress, setIpAddress]: [string, React.Dispatch<React.SetStateAction<string>>] = React.useState('')
+    const [clusterName, setClusterName]: [string, React.Dispatch<React.SetStateAction<string>>] = React.useState('')
+  
+    const { componentState, setComponentState }:StateContextType = useContext(StateContext)
+    const handleIpAddressChange = (e: ChangeEvent<HTMLInputElement>): void => {
+      setIpAddress(e.target.value);
+    };
+    const handleClusterNameChange = (e: ChangeEvent<HTMLInputElement>): void => {
+      console.log(e.target.value)
+      setClusterName(e.target.value);
+    };
+    const closeHandler = () => {
+        setComponentState((prevState: AppState) => {
+          const { clusters } = prevState;
+          const newCluster = { ipAddress, clusterName };
+          const newClusters = Array.isArray(clusters) ? [...clusters, newCluster] : [newCluster];
+          return {
+            ...prevState,
+            clusters: newClusters,
+            currentTab: {
+              ipAddress: ipAddress,
+              clusterName: clusterName
+            }
+          };
+        });
+        setVisible(false);
+      };
+      
+  
+    
+    return (
+      <div>
+        <Button auto shadow onPress={handler}>
+          Add Clusters
+        </Button>
+        <Modal
+          closeButton
+          aria-labelledby="modal-title"
+          open={visible}
+          onClose={() => setVisible(false)}
+        >
+          <Modal.Header>
+            <Text id="modal-title" size={18}>
+              KubeHawk
+              <Text b size={18}>
+               
+              </Text>
             </Text>
-          </Text>
-        </Modal.Header>
-        <Modal.Body>
-          <Input
-            clearable
-            bordered
-            fullWidth
-            color="primary"
-            size="lg"
-            placeholder="Grafana Endpoint"
-            aria-label="Grafana Endpoint"
-            onChange={handleIpAddressChange}
-          />
-          <Input
-            clearable
-            bordered
-            fullWidth
-            color="primary"
-            size="lg"
-            placeholder="Cluster Name"
-            aria-label="Cluster Name"
-            onChange={handleClusterNameChange}
-          />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button auto flat color="error" onPress={closeHandler} aria-label="Close">
-            Close
-          </Button>
-          <Button auto onPress={() => {
-            closeHandler}} aria-label="Add a cluster">
-            Add a cluster
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </div>
-  );
-}
+          </Modal.Header>
+          <Modal.Body>
+            <Input
+              clearable
+              bordered
+              fullWidth
+              color="primary"
+              size="lg"
+              placeholder="Grafana Endpoint"
+              aria-label="Grafana Endpoint"
+              onChange={handleIpAddressChange}
+            />
+            <Input
+              clearable
+              bordered
+              fullWidth
+              color="primary"
+              size="lg"
+              placeholder="Cluster Name"
+              aria-label="Cluster Name"
+              onChange={handleClusterNameChange}
+            />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button auto flat color="error" onPress={() => closeHandler()} aria-label="Close">
+              Close
+            </Button>
+            <Button auto onPress={() => 
+              closeHandler()} aria-label="Add a cluster">
+              Add a cluster
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+  
