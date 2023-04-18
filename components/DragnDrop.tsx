f8e354dcfac8b6ab30985cc7668154b36793638c
sffw <spencerffwhite@gmail.com>
2023-04-18 16:14:12 -0400

    Add D3 charts

---
+++ components/DragnDrop.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { FileUploader } from 'react-drag-drop-files';
+import SunburstChart from './charts/SunburstChart';
+import TreeGraph from './charts/TreeGraph';
+import ForceGraph from './charts/ForceGraph';
+
+
+const fileTypes = ['JSON', 'TXT'];
+
+function DragnDrop() {
+  const [file, setFile] = useState(null);
+  const [data, setData] = useState();
+  const [sun, setSun] = useState(false);
+  const [tree, setTree] = useState(false);
+  const [force, setForce] = useState(false);
+
+  const handleChange = (file) => {
+    setFile(file);
+
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      console.log('event.target.result');
+      setData(event.target.result)
+    };
+    reader.readAsText(file);
+  };
+
+  const handleSun = () => {
+    console.log(data)
+    setSun(true);
+  }
+  const handleTree = () => {
+    console.log(data)
+    setTree(true);
+  }
+  const handleForce = () => {
+    console.log(data)
+    setForce(true);
+  }
+
+  return (
+    <>
+    <h1>Drag and Dropper</h1>
+      <FileUploader handleChange={handleChange} name='file' types={fileTypes} />
+      
+      {data && <button onClick={handleSun}>Load the Sunburst Chart</button>}
+      {data && <button onClick={handleTree}>Load the Tree Graph</button>}
+      {data && <button onClick={handleForce}>Load the Force Chart</button>}
+
+      {sun && <SunburstChart roles={JSON.parse(data)} />}
+      {tree && <TreeGraph roles={JSON.parse(data)} />}
+      {force && <ForceGraph roles={JSON.parse(data)} />}
+    </>
+  );
+}
+
+export default DragnDrop;
---
+++ pages/dragndrop.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import DragnDrop from '../components/DragnDrop';
+
+
+const dragndrop = () => {
+   return <DragnDrop />;
+};
+
+export default dragndrop;
