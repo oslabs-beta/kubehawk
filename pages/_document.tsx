c94cc84a4d3bcc9215561753138e68b232ba1130
stevenox7 <101216881+stevenox7@users.noreply.github.com>
2023-04-19 13:37:44 -0400

    Added heading style

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,7 +24,8 @@ class MyDocument extends Document {
       return (
          <Html lang="en">
             <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <title>Kubehawk</title>
+            <title>KubeHawk</title>
+            <link rel="icon" href="/kubehawklog.ico" />
             <link
                href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
                rel="stylesheet"
